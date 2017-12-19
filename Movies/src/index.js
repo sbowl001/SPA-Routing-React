9f433c1953bad3247fb6b92bfbb2e1c744465527
Stephanie <sbowl001@fiu.edu>
2017-12-19 23:05:57 +0900

    first

--- Movies/src/index.js
+++ Movies/src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';  
+
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore); //??
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/movies" component={Movies} />
+        <Route path="/movies/:id" component={MovieDetail} />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);  
